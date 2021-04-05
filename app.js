326558a8b1ba94e4e3b634707be5b0146a6408dc
Dominika <dominika.moravicova@gmail.com>
2021-04-05 19:43:41 +0200

    make it better

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const mongoose = require('mongoose');
+
+const graphQlSchema = require('./graphql/schema/index');
+const graphQlResolvers = require('./graphql/resolvers/index');
+const isAuth = require('./middleware/is-auth');
+require('dotenv').config();
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
+app.use(isAuth);
+
+app.use(
+  '/graphql',
+  graphqlHttp({
+    schema: graphQlSchema,
+    rootValue: graphQlResolvers,
+    graphiql: true
+  })
+);
+
+mongoose.connect("mongodb+srv://"+ process.env.MONGO_ATLAS_PW +"@hotel-reserve-sys.vhpfr.mongodb.net/hotel-reserve-sys?retryWrites=true&w=majority",{ useNewUrlParser: true, useUnifiedTopology: true }
+	).then(() => {
+		app.listen(process.env.PORT || 3000);
+		console.log("pripojeny");
+	}).catch(err => {
+		console.log(err);
+	});
+
+
---
+++ graphql/resolvers/index.js
@@ -0,0 +1,17 @@
+const authResolver = require('./auth');
+const eventsResolver = require('./events');
+const bookingResolver = require('./booking');
+
+const rootResolver = {
+  ...authResolver,
+  ...eventsResolver,
+  ...bookingResolver
+};
+
+module.exports = rootResolver;
+
+
+
+
+
+
